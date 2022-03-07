3a2d6c340ace23141116ebbeab875df6db331410
xalphonseinbaraj <47241538+xalphonseinbaraj@users.noreply.github.com>
2022-03-07 22:38:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Face Recogition using Twin Neural Network'
 LONG_DESCRIPTION = 'A package that allows to reconize the face using Siamese Neural Network.'
 
